ce179f27947fe73219c0c03a67977cca6aa58693
Matthew McFarland <mmcfarland@azavea.com>
2013-08-21 14:26:29 -0400

    Comment Get/Set based on roles
    Add and Get comments based on user roles

--- csharp/Urban.DCP.Data/Comment.cs
+++ csharp/Urban.DCP.Data/Comment.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Azavea.Database;
 using Azavea.Open.Common;
+using Azavea.Open.DAO.Criteria;
 
 namespace Urban.DCP.Data
 {
@@ -13,13 +15,39 @@ namespace Urban.DCP.Data
             new FastDAO<Comment>(Config.GetConfig("PDP.Data"), "PDB");
 
         public int Id;
-        public int NlihcId;
+        /// <summary>
+        /// Property this comment is attached to
+        /// </summary>
+        public string NlihcId;
+
+        /// <summary>
+        /// Access level which this comment is viewable
+        /// </summary>
         public CommentAccessLevel AccessLevel;
+
+        /// <summary>
+        /// If AccessLevel is set to Org mode, the org in question
+        /// </summary>
+        public int? AssociatedOrgId;
         public DateTime Created;
         public DateTime Modified;
         public string Username;
+
+        /// <summary>
+        /// Admin users can modify comments, this tracks who last
+        /// edited the 
+        /// </summary>
         public string LastEditorId;
+
+        /// <summary>
+        /// Optional Image attached to comment
+        /// </summary>
         public byte[] Image;
+
+        /// <summary>
+        /// Text value of comment
+        /// </summary>
+        public string Text;
 
         public User User {
             get { return UserHelper.GetUser(Username); }
@@ -30,5 +58,67 @@ namespace Urban.DCP.Data
             get { return UserHelper.GetUser(LastEditorId); }
         }
 
+        public static void AddComment(string nlihcId, User user, 
+            CommentAccessLevel level, string text, byte[] image)
+        {
+            var created = DateTime.Now;
+            var comment = new Comment
+                {
+                    NlihcId = nlihcId,
+                    AccessLevel = level,
+                    AssociatedOrgId = level == CommentAccessLevel.Network ? user.Organization : null,
+                    Created = created,
+                    Modified = created,
+                    Username = user.UserName,
+                    Text = text,
+                    Image = image
+                };
+
+            _dao.Insert(comment);
+        }
+
+        public static IList<Comment> GetAuthorizedComments(string nlihcId, User user)
+        {
+            var crit = new DaoCriteria();
+            crit.Expressions.Add(new EqualExpression("NlihcId", nlihcId));
+
+            // Anonymous users get only public comments
+            if (user == null)
+            {
+                crit.Expressions.Add(new EqualExpression("AccessLevel", CommentAccessLevel.Public));
+                return _dao.Get(crit);
+            }
+
+            // This kind of query is difficult in FastDAO, so, expecting that the number
+            // of comments on a given property will be reasonable, prune off unauthorized
+            // comments from the entire property comment list
+            var comments = _dao.Get(crit);
+
+            // SysAdmins can see everything
+            if (user.IsSysAdmin())
+            {
+                return comments;
+            }
+
+            var authComments = new List<Comment>();
+            foreach (var comment in comments)
+            {
+                switch (comment.AccessLevel)
+                {
+                    case CommentAccessLevel.Public:
+                        authComments.Add(comment);
+                        break;
+                    case CommentAccessLevel.Network:
+                        if (user.IsNetworked()) authComments.Add(comment);
+                        break;
+                    case CommentAccessLevel.SameOrg:
+                        if (user.Organization == comment.AssociatedOrgId) authComments.Add(comment);
+                        break;
+                }
+            }
+
+            return authComments;
+
+        }
     }
 }
