7812530b264491f20a1ef9a41c8f45a9b6ebd243
Matthew McFarland <mmcfarland@azavea.com>
2013-08-29 09:32:44 -0400

    Add logger handler for client logs

---
+++ csharp/Urban.DCP.Handlers/LoggerHandler.cs
@@ -0,0 +1,9 @@
+﻿﻿using System.Web.Services;
+using Azavea.Web.Handler;
+
+namespace Urban.DCP.Handlers
+{
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    public class Logger : LoggerHandler {}
+}
